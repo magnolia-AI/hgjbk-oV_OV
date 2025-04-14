71482c7b2ab6b16e6e07bd73a090faa381054f0c
magnolia-AI <github@antonmagnusson.se>
2025-04-14 12:26:12 +0200

    Update 1 files

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@ import Image from 'next/image';
 
     export default function Home() {
       return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
           <Image 
             src="https://storage.googleapis.com/magnolia-storage-dev-test-123/projects/4e9dc240-fccc-415a-adf7-c3a5694fbf2f/cm92utiac0000a8z95mz5asqt/images/cnt3ldnhwq.png" 
             alt="Stylized Abstract Illustration" 
+            width={512} 
+            height={512} 
+          />
+          <Image 
+            src="https://storage.googleapis.com/magnolia-storage-dev-test-123/images/emraxlcen5/elderly_man_recycling.png" 
+            alt="Elderly Man in Recycling Bin" 
             width={512} 
             height={512} 
           />
